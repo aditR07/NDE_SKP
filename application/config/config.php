9cc834a6fb27054487f4341d1897062e723d9e6c
nde <aditiarahmat07@gmail.com>
2024-10-09 10:24:45 +0700

    Initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pengarsipan/';
+$config['base_url'] = 'http://localhost/NDE_SKP/';
 //$config['base_url'] = 'http://sisarsip-smkdh.now.sh';
 
 /*
